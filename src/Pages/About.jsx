435ecdb93f44e7fcff54ab3102a0e16835dcafae
krishnachaturvedi1901 <krishna.chaturvedi1901@gmail.com>
2023-07-15 20:13:46 +0530

    done full assignment accept contact page remaining

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -11,41 +11,91 @@ const About = () => {
             Lets discuss about <span>Projects</span>
           </h1>
           <h3>
-            <strong>I have completed total of 4 fullstack clones using MERN stack and 20+ mini projects during my certification journey.</strong>
+            <strong>
+              I have completed total of 4 fullstack clones using MERN stack and
+              20+ mini projects during my certification journey.
+            </strong>
           </h3>
+          <h4>Please checkout my <a href="http://www.krishnachaturvedi.tech" target="_blank" >Portfolio</a> and github for all projects</h4>
         </div>
         <img src={welcomeImg} />
       </section>
-      <section className={styles.introductionSection} >
+
+      <section className={styles.introductionSection}>
         <h1>Project Details</h1>
-        <div>
-          <ul>
-            <li>I belongs from Jhansi(U.P.)</li>
-            <li>
-              Iam a gradguate and completed my fullstack certification from{" "}
-              <strong>Masai school</strong> this year.
-            </li>
-            <h3>Skills</h3>
-            <li>
-              I know<strong> HTML, CSS ,Javscript ,React, Nodejs, Express, MongoDB,
-              ChakraUI </strong> and using these techstack i have created 2-3 clones and
-              lots of mini projects during my certification in masai school.
-            </li>
-            <li>
-              I have solved nearly 400 questions in DSA using <strong>JAVA</strong> which
-              enhances my logic building and helped lot in solving problems
-              during development.
-            </li>
-            <h3>Projects</h3>
-            <li>I have done my project discussion in <a href="/about" >About</a> page </li>
-            <h3>Past Experience</h3>
-            <li>I have worked more then a year in Gurgao based startup 'Goflydigital.pvt.ltd' as a <strong> Character artist and designer</strong> after that i started learning fullstack development.</li>
-            <h3>Hobbies & Intrests</h3>
-            <li>I love storts and fitness and pursue doing it sideways.</li>
-            <li>Iam a <strong>Taekwondo state gold medalist</strong> and Volleyball Inter-university champion in my college time</li>
-            <li>Right now iam learning Table-tennis in evening</li>
-            <h1>Thankyou! &#128512; </h1>
-          </ul>
+        <div className={styles.oneProjectDiv}>
+          <div className={styles.left}>
+            <div className={styles.img}>
+              <img src="https://i.imgur.com/qzZ86v4.jpg" alt="" />
+            </div>
+          </div>
+          <div className={styles.right}>
+            <h2 className={styles.projectTitle}>
+              Full-stack Blissworld Clone{" "}
+            </h2>
+
+            <p className={styles.projectDesc}>
+              <div className={styles.featureDiv} >
+                Build clone of blissworld in 7 days along with a team 4 members.
+                <h3>Features</h3>
+                <ul style={{ marginLeft: "30px" }}>
+                  <li>Login</li>
+                  <li>Signup</li>
+                  <li>Product page</li>
+                  <li>Product detail page</li>
+                  <li>Cart Page</li>
+                  <li>Checkout Page</li>
+                  <li>Order page</li>
+                </ul>
+              </div>
+              <div className={styles.techStackDiv} >
+                <h2>
+                  <b>
+                    <i> Tech Stack :</i>
+                  </b>{" "}
+                  Reactjs | ChakraUI | Nodejs | Express | MongoDB | Mongoose
+                </h2>
+                <br/>
+                <h2>
+                  <b>
+                    <i> Area of Responsibility :</i>
+                  </b>{" "}
+                  Developed the cart page with all CRUD properties of adding
+                  product to cart , update and delete products from cart.
+                </h2>
+              </div>
+            <div className={styles.buttons}>
+              <a
+                href="https://github.com/krishnachaturvedi1901/Blissworld_MERN_Fullstack_Clone"
+                target="_blank"
+                className={styles.primaryBtn}
+              >
+                GitHub
+              </a>
+              <a
+                href="https://blissworld-fullstack-clone.netlify.app"
+                target="_blank"
+                className={styles.primaryBtn}
+              >
+                Preview{" "}
+              </a>
+              <a
+                href="https://blissworld-mern-fullstack-clone.hashnode.dev/mern-full-stack-clone"
+                target="_blank"
+                className={styles.primaryBtn}
+              >
+                Blog{" "}
+              </a>
+              <a
+                href="https://blissworld-mern-fullstack-clone.hashnode.dev/mern-full-stack-clone"
+                target="_blank"
+                className={styles.primaryBtn}
+              >
+                Demo{" "}
+              </a>
+            </div>
+            </p>
+          </div>
         </div>
       </section>
     </header>
